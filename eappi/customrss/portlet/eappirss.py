e8e6a831db120009e581ed2bab56dcbd7c7bed05
Khairul Anwar (kulll) <anwarbaik88@gmail.com>
2014-09-23 17:19:52 +0800

    working custom rss

--- eappi/customrss/portlet/eappirss.py
+++ eappi/customrss/portlet/eappirss.py
@@ -20,37 +20,78 @@ from Acquisition import aq_inner
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from eappi.customrss import MessageFactory as _
 
-class IEappiRSS(IPortletDataProvider):
+from plone.app.portlets.portlets import rss
+from DateTime import DateTime
+from DateTime.interfaces import DateTimeError
+
+
+FEED_DATA = {}
+
+
+class RSSFeed(rss.RSSFeed):
+    def _buildItemDict(self, item):
+        link = item.links[0]['href']
+        picture = item.get('media_content', '')
+        if len(picture) > 1:
+            picture = picture[1].get('url')
+        else:
+            picture = ''
+        itemdict = {
+            'title': item.title,
+            'url': link,
+            'summary': item.get('description', ''),
+            'media':  picture,
+        }
+        if hasattr(item, "updated"):
+            try:
+                itemdict['updated'] = DateTime(item.updated)
+            except DateTimeError:
+                # It's okay to drop it because in the
+                # template, this is checked with
+                # ``exists:``
+                pass
+
+        return itemdict
+
+
+class IEappiRSS(rss.IRSSPortlet):
     """
-    Define your portlet schema here
+    This portlet is subclass of rss from plone.app.porlets
     """
     pass
 
-class Assignment(base.Assignment):
+
+class Assignment(rss.Assignment):
     implements(IEappiRSS)
 
     def __init__(self, **kwargs):
         for k, v in kwargs.items():
             setattr(self, k, v)
 
-    @property
-    def title(self):
-        return _('Eappi RSS')
 
-class Renderer(base.Renderer):
-    
+class Renderer(rss.Renderer):
+
     render = ViewPageTemplateFile('templates/eappirss.pt')
+
+    def _getFeed(self):
+        """return a feed object but do not update it"""
+        feed = FEED_DATA.get(self.data.url, None)
+        if feed is None:
+            # create it
+            feed = FEED_DATA[self.data.url] = RSSFeed(self.data.url, self.data.timeout)
+        return feed
 
     @property
     def available(self):
         return True
 
+
 # XXX: z3cform
 # class AddForm(z3cformhelper.AddForm):
-class AddForm(base.AddForm):
+class AddForm(rss.AddForm):
 
-#    XXX: z3cform
-#    fields = field.Fields(IEappiRSS)
+    #  XXX: z3cform
+    #  fields = field.Fields(IEappiRSS)
 
     form_fields = form.Fields(IEappiRSS)
 
@@ -60,12 +101,13 @@ class AddForm(base.AddForm):
     def create(self, data):
         return Assignment(**data)
 
+
 # XXX: z3cform
 # class EditForm(z3cformhelper.EditForm):
-class EditForm(base.EditForm):
+class EditForm(rss.EditForm):
 
-#    XXX: z3cform
-#    fields = field.Fields(IEappiRSS)
+    # XXX: z3cform
+    # fields = field.Fields(IEappiRSS)
 
     form_fields = form.Fields(IEappiRSS)
 
